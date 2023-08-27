ed7f0d4b29698bb4087fd25e89d6c3bdfecef71c
Raj <rajeshkumarsahoo006@gmail.com>
2023-08-27 19:12:53 +0530

    Initial Push

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors"); // Fix typo here
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/UserRouters");
+
+const app = express();
+
+app.use(cors()); // Fix typo here
+app.use(express.json());
+
+mongoose
+  .connect("mongodb+srv://raj_3028:SoVKWYnFbvC7Z9Ic@cluster0.pw23ckf.mongodb.net/netflix", {
+    useNewUrlParser: true, // Fix typo here
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database:", err);
+  });
+
+app.use("/api/user", userRoutes);
+
+app.listen(5000, console.log("Server started on port 5000"));
