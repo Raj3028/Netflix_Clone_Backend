c295767c5063a491b4d18c6c07f495155fabd248
Raj <rajeshkumarsahoo006@gmail.com>
2023-08-27 19:19:11 +0530

    Initial Push

--- routes/UserRouters.js
+++ routes/UserRouters.js
@@ -10,4 +10,8 @@ router.get("/liked/:email", getLikedMovies);
 router.post("/add", addToLikedMovies);
 router.put("/remove", removeFromLikedMovies);
 
+router.get('/test', (req, res)=>{
+  return res.status(200).json({status:true, message: "Server run successfully"})
+})
+
 module.exports = router;
